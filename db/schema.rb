4457ff2de563651c77b10cd6ced42f5cce022071
laura reimarte <reimarte.laura@gmail.com>
2022-07-27 21:42:00 +0100

    list route controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_203258) do
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "overview"
+    t.string "poster_url"
+    t.decimal "raiting"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
